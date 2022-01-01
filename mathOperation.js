71bf8f51370b8a5d84854e13e7bb113fae0148b0
Farid118 <faridmasjedi118@gmail.com>
2022-01-01 23:18:56 +1100

    Update

--- mathOperation.js
+++ mathOperation.js
@@ -248,7 +248,7 @@ class MathOperation{
     }
 
     division = (num, dividedBy) => {
-        if (typeof num1 !== 'string' || typeof num2 !== 'string') { 
+        if (typeof num1 !== 'string' || typeof dividedBy !== 'string') { 
             return 'the inputs should be in string type.'
         }
 
